f3e64fc4ba34c4de84cd7ab012d8530c9ab4850d
ArtemyDrzhn <artemdrzhn@gmail.com>
2021-02-17 20:44:45 +0300

    Сделал загрузку файла (пока без привязки к модели)

--- Seti/social_network/urls.py
+++ Seti/social_network/urls.py
@@ -1,6 +1,11 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.model_form_upload, name='upload'),
+    path('upload/', views.model_form_upload, name='upload'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
